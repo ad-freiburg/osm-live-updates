e32630c48d61387a7e93a255d47e1d0a48bfd01d
Nicolas von Trott <nicolasvontrott@gmail.com>
2024-07-23 16:57:55 +0200

    Added Test for Query Writer

---
+++ tests/sparql/QueryWriter.cpp
@@ -0,0 +1,56 @@
+// Copyright 2024, University of Freiburg
+// Authors: Nicolas von Trott <nicolasvontrott@gmail.com>.
+
+// This file is part of osm-live-updates.
+//
+// osm-live-updates is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm-live-updates is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with osm-live-updates.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "osm-live-updates/sparql/QueryWriter.h"
+#include "osm-live-updates/config/Constants.h"
+#include "gtest/gtest.h"
+
+namespace olu::sparql {
+    TEST(QueryWriter, insert) {
+        {
+            olu::sparql::QueryWriter queryWriter;
+            std::vector<std::string> triples;
+            triples.emplace_back("osmrel:1960198 ogc:sfContains ?osm_id:10559440 .");
+
+            std::string query = queryWriter.writeInsertQuery(triples);
+            ASSERT_EQ(
+                    olu::config::constants::PREFIXES +
+                    "INSERT DATA {\n"
+                    "osmrel:1960198 ogc:sfContains ?osm_id:10559440 .\n"
+                    "}",
+                    query
+            );
+        }
+        {
+            olu::sparql::QueryWriter queryWriter;
+            std::vector<std::string> triples;
+            triples.emplace_back("osmrel:1960198 ogc:sfContains ?osm_id:10559440 .");
+            triples.emplace_back("region:102740 osmkey:name name:Bretagne .");
+
+            std::string query = queryWriter.writeInsertQuery(triples);
+            ASSERT_EQ(
+                    olu::config::constants::PREFIXES +
+                    "INSERT DATA {\n"
+                    "osmrel:1960198 ogc:sfContains ?osm_id:10559440 .\n"
+                    "region:102740 osmkey:name name:Bretagne .\n"
+                    "}",
+                    query
+            );
+        }
+    }
+}
