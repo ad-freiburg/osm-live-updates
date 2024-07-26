888da5488829f70853ddc189a2b47859a7de3f09
Nicolas von Trott <nicolasvontrott@gmail.com>
2024-07-26 15:01:29 +0200

    Added Function to fetch the sequence number of the latest diff

--- include/osm-live-updates/OsmDataFetcher.h
+++ include/osm-live-updates/OsmDataFetcher.h
@@ -5,10 +5,14 @@
 #ifndef OSM_LIVE_UPDATES_OSMDATAFETCHER_H
 #define OSM_LIVE_UPDATES_OSMDATAFETCHER_H
 
+#include <string>
+
 namespace olu {
 
 class OsmDataFetcher {
 public:
+    // Fetches the sequence number of the latest diff from the osm server and returns it
+    static std::string fetchLatestSequenceNumber();
     static void fetchDiffWithSequenceNumber(int& sequenceNumber);
 };
 
--- src/osm-live-updates/OsmDataFetcher.cpp
+++ src/osm-live-updates/OsmDataFetcher.cpp
@@ -17,34 +17,44 @@
 // along with osm-live-updates.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "osm-live-updates/OsmDataFetcher.h"
+#include "osm-live-updates/config/Constants.h"
+#include "osm-live-updates/util/URLHelper.h"
+#include "osm-live-updates/util/HttpClient.h"
 
-#include <curl/curl.h>
-#include <iostream>
 #include <string>
+#include <vector>
+#include <boost/regex.hpp>
 
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
+namespace constants = olu::config::constants;
 
 namespace olu {
 
 // _________________________________________________________________________________________________
-void OsmDataFetcher::fetchDiffWithSequenceNumber(int &sequenceNumber) {
-    CURL *curl;
-    CURLcode res;
-    std::string readBuffer;
+std::string OsmDataFetcher::fetchLatestSequenceNumber() {
+    // Build url for state file
+    std::vector<std::string> pathSegments;
+    pathSegments.emplace_back(constants::OSM_REPLICATION_BASE_URL);
+    pathSegments.emplace_back("minute");
+    pathSegments.emplace_back(constants::OSM_DIFF_STATE_FILE + constants::TXT_EXTENSION);
+    std::string url = util::URLHelper::buildUrl(pathSegments);
 
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
+    // Get state file from osm server
+    std::string readBuffer = util::HttpClient::perform_get(url);
 
-        std::cout << readBuffer << std::endl;
+    // Extract sequence number from state file
+    boost::regex regex("sequenceNumber=(\\d+)");
+    boost::smatch match;
+    if (boost::regex_search(readBuffer, match, regex)) {
+        std::string number = match[1];
+        return number;
+    } else {
+       // TODO: Throw error if nothing found.
     }
+}
+
+// _________________________________________________________________________________________________
+void OsmDataFetcher::fetchDiffWithSequenceNumber(int &sequenceNumber) {
+
 }
 
 } // namespace olu
