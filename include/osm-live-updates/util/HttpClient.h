18961097adc2ab2b8b34744acb018735a2f887f7
Nicolas von Trott <nicolasvontrott@gmail.com>
2024-07-26 14:41:40 +0200

    Helper class for Http Requests

---
+++ include/osm-live-updates/util/HttpClient.h
@@ -0,0 +1,33 @@
+// Copyright 2024, University of Freiburg
+// Authors: Nicolas von Trott <nicolasvontrott@gmail.com>.
+
+// This file is part of osm-live-updates.
+//
+// osm-live-updates is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm-live-updates is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with osm-live-updates.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef OSM_LIVE_UPDATES_HTTPCLIENT_H
+#define OSM_LIVE_UPDATES_HTTPCLIENT_H
+
+#include <string>
+
+namespace olu::util {
+
+class HttpClient {
+public:
+    static std::string perform_get(std::string& url);
+};
+
+} // namespace olu::util
+
+#endif //OSM_LIVE_UPDATES_HTTPCLIENT_H
---
+++ src/util/HttpClient.cpp
@@ -0,0 +1,58 @@
+// _________________________________________________________________________________________________
+
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/boostorg/beast
+//
+
+//------------------------------------------------------------------------------
+//
+// Example: HTTP client, synchronous for every method on httpbin
+//
+//------------------------------------------------------------------------------
+
+#include "osm-live-updates/util/HttpClient.h"
+
+#include <string>
+#include <curl/curl.h>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+// Performs an HTTP request against httpbin.cpp.al and prints request & response
+namespace olu::util {
+
+// _________________________________________________________________________________________________
+std::string HttpClient::perform_get(std::string& url) {
+    CURL *curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        }
+
+        curl_easy_cleanup(curl);
+
+        return readBuffer;
+    }
+}
+
+// _________________________________________________________________________________________________
+
+
+} // namespace olu::util
