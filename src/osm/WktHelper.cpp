d820e5f933cafa1d87c7c6a4d101d32de0175b82
Nicolas von Trott <nicolasvontrott@gmail.com>
2024-08-28 14:37:28 +0200

    - Added Class for Handling WKT Format - Added function + test to create a dummy node from a WKT point

---
+++ src/osm/WktHelper.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Nicolas von Trott on 28.08.24.
+//
+
+#include "osm/WktHelper.h"
+
+#include <boost/regex.hpp>
+#include <string>
+
+std::string olu::osm::WktHelper::createDummyNodeFromPoint(const std::string& nodeId,
+                                                          const std::string& pointAsWkt) {
+    const boost::regex pattern(R"(POINT\(([-+]?[0-9]*\.?[0-9]+)\s+([-+]?[0-9]*\.?[0-9]+)\))");
+    boost::smatch match;
+
+    std::string lat;
+    std::string lon;
+    if (boost::regex_search(pointAsWkt, match, pattern)) {
+        lon = match[1];
+        lat = match[2];
+    } else {
+        throw WktHelperException(("No WKT Point found in " + pointAsWkt).c_str());
+    }
+
+    return "<node id=\"" + nodeId + "\" lat=\"" + lat + "\" lon=\"" + lon + "\">";
+}
---
+++ tests/osm/WktHelper.cpp
@@ -0,0 +1,63 @@
+// Copyright 2024, University of Freiburg
+// Authors: Nicolas von Trott <nicolasvontrott@gmail.com>.
+
+// This file is part of osm-live-updates.
+//
+// osm-live-updates is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osm-live-updates is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with osm-live-updates.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "osm/WktHelper.h"
+#include "gtest/gtest.h"
+
+TEST(WktHelper, createDummyNodeFromPoint) {
+    {
+        std::string pointAsWkt = "POINT(13.5690032 42.7957187)";
+        std::string nodeId = "1";
+
+        auto dummyNode = olu::osm::WktHelper::createDummyNodeFromPoint(nodeId, pointAsWkt);
+        ASSERT_EQ(dummyNode,
+                  "<node id=\"1\" lat=\"42.7957187\" lon=\"13.5690032\">");
+    }
+    {
+        std::string pointAsWkt = "POINT(42.7957187)";
+        std::string nodeId = "1";
+
+        ASSERT_THROW(
+                olu::osm::WktHelper::createDummyNodeFromPoint(nodeId, pointAsWkt),
+                olu::osm::WktHelperException);
+    }
+    {
+        std::string pointAsWkt = "POINT(13.5690032,42.7957187)";
+        std::string nodeId = "1";
+
+        ASSERT_THROW(
+                olu::osm::WktHelper::createDummyNodeFromPoint(nodeId, pointAsWkt),
+                olu::osm::WktHelperException);
+    }
+    {
+        std::string pointAsWkt = "(13.5690032 42.7957187)";
+        std::string nodeId = "1";
+
+        ASSERT_THROW(
+                olu::osm::WktHelper::createDummyNodeFromPoint(nodeId, pointAsWkt),
+                olu::osm::WktHelperException);
+    }
+    {
+        std::string pointAsWkt = "13.5690032 42.7957187";
+        std::string nodeId = "1";
+
+        ASSERT_THROW(
+                olu::osm::WktHelper::createDummyNodeFromPoint(nodeId, pointAsWkt),
+                olu::osm::WktHelperException);
+    }
+}
