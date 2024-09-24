2309eb87775f456fed5775e892f647732c18b1e0
Nicolas von Trott <nicolasvontrott@gmail.com>
2024-09-24 11:31:50 +0200

    - Added Documentation

--- src/osm/OsmUpdater.cpp
+++ src/osm/OsmUpdater.cpp
@@ -11,32 +11,39 @@ namespace olu::osm {
     : _config(config), _odf(OsmDataFetcher(config)), _och(OsmChangeHandler(config)) { }
 
     void OsmUpdater::run() {
+        // If the user has provided the path to an osm change file, only this change file will be
+        // processed
         if (!(_config.pathToOsmChangeFile.empty())) {
             std::cout
             << osm2rdf::util::currentTimeFormatted()
             << "Start handling change file: "
             << std::endl;
+
             _och.handleChange(_config.pathToOsmChangeFile, false);
         } else {
             _latestState = _odf.fetchLatestDatabaseState();
             auto sequenceNumber = decideStartSequenceNumber();
+
             std::cout
             << osm2rdf::util::currentTimeFormatted()
             << "Start at sequence number: "
             << sequenceNumber
             << std::endl;
+
             std::cout
             << osm2rdf::util::currentTimeFormatted()
             << "Latest sequence number: "
             << _latestState.sequenceNumber
             << std::endl;
+
             while (sequenceNumber <= _latestState.sequenceNumber) {
                 auto pathToOsmChangeFile = _odf.fetchDiffWithSequenceNumber(sequenceNumber);
                 std::cout
                 << osm2rdf::util::currentTimeFormatted()
-                << "Handling diff with sequence number: "
+                << "Handling change file for sequence number: "
                 << sequenceNumber
                 << std::endl;
+
                 _och.handleChange(pathToOsmChangeFile, true);
                 sequenceNumber++;
             }
