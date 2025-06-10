a1f923116a009eee54080a90332f19ae3f5e2aad
Nicolas von Trott <nicolasvontrott@gmail.com>
2025-06-10 14:41:29 +0200

    - Added File Header

--- benchmarks/util/TtlHelper.cpp
+++ benchmarks/util/TtlHelper.cpp
@@ -1,6 +1,20 @@
+// Copyright 2025, University of Freiburg
+// Authors: Nicolas von Trott <nicolasvontrott@gmail.com>.
+
+// This file is part of osm-live-updates.
 //
-// Created by Nicolas von Trott on 20.05.25.
+// osm-live-updates is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// osm-live-updates is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with osm-live-updates.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "benchmark/benchmark.h"
 #include "util/TtlHelper.h"
